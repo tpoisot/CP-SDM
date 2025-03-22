ef5e5ec2fb0410e10b608798938ce12a92f840d2
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-22 12:55:05 -0400

    up

--- main.jl
+++ main.jl
@@ -17,7 +17,8 @@ presences = occurrences(
     sp,
     first(L),
     "occurrenceStatus" => "PRESENT",
-    "limit" => 300
+    "limit" => 300,
+    "datasetKey" => "4fa7b334-ce0d-4e88-aaae-2e0c138d049e",
 )
 while length(presences) < min(count(presences), 2_000)
     occurrences!(presences)
@@ -25,31 +26,46 @@ end
 
 presencelayer = mask(first(L), presences)
 background = pseudoabsencemask(DistanceToEvent, presencelayer)
-bgpoints = backgroundpoints(nodata(background, d -> d < 2), 3sum(presencelayer))
+bgpoints = backgroundpoints(nodata(background, d -> d < 3), 2sum(presencelayer))
 
-poly(CHE)
-scatter!(presencelayer)
-scatter!(bgpoints)
+poly(CHE, color=:grey80)
+scatter!(presencelayer, color=:red)
+scatter!(bgpoints, color=:grey30, markersize=3)
+lines!(CHE, color=:black)
 current_figure()
 
 # Set up the model
 sdm = SDM(ZScore, Logistic, L, presencelayer, bgpoints)
-hyperparameters!(classifier(sdm), :η, 1e-3);
+hyperparameters!(classifier(sdm), :η, 5e-4);
 hyperparameters!(classifier(sdm), :interactions, :all);
-hyperparameters!(classifier(sdm), :epochs, 5_000);
+hyperparameters!(classifier(sdm), :epochs, 8_000);
 
 # Train the model with optimal set of variables
 variables!(sdm, ForwardSelection; included=[1], verbose=true)
+#train!(sdm)
 
 ConfusionMatrix(sdm) |> mcc
 
 prd = predict(sdm, L; threshold=false)
 heatmap(prd)
+lines!(CHE, color=:black)
+current_figure()
+
+# VI
+vi = variableimportance(sdm, [holdout(sdm)]; threshold=false)
+miv = variables(sdm)[last(findmax(vi))]
+
+scatter(features(sdm, 1), predict(sdm; threshold=false))
+hlines!([threshold(sdm)], color=:red)
+current_figure()
+
+# Range
+distrib = predict(sdm, L)
 
 cs = cellsize(prd)
 
 cmodel = deepcopy(sdm)
-q = _estimate_q(cmodel, holdout(cmodel)...; α=0.05)
+q = _estimate_q(cmodel, holdout(cmodel)...; α=0.005)
 
 rlevels = LinRange(0.01, 0.2, 25)
 qs = [_estimate_q(cmodel, holdout(cmodel)...; α=u) for u in rlevels]
@@ -58,9 +74,24 @@ scatter(rlevels, qs)
 Cp, Ca = credibleclasses(prd, q)
 heatmap(Ca .& Cp, colorrange=(0, 1))
 
-poly(CHE, color=:white, strokecolor=:black, strokewidth=1)
-heatmap!(nodata(Cp, false), colormap=[:transparent, :grey80])
-heatmap!(nodata(Ca .& Cp, false), colormap=[:transparent, :grey20])
-scatter!(presencelayer, markersize=3, color=:orange)
-#contour!(predict(sdm, L); color=:red)
+# Partition
+sure_presence = Cp .& (.!Ca)
+heatmap(sure_presence)
+sure_absence = Ca .& (.!Cp)
+heatmap(sure_absence)
+unsure = Ca .& Cp
+heatmap(unsure)
+unsure_in = unsure .& distrib
+heatmap(unsure_in)
+unsure_out = unsure .& (.!distrib)
+heatmap(unsure_out)
+
+poly(CHE, color=:grey90; axis=(; aspect=DataAspect()), figure=(; size=(1000, 550)))
+heatmap!(nodata(sure_presence, false), colormap=[:transparent, :black, :black])
+heatmap!(nodata(unsure_in, false), colormap=[:transparent, :forestgreen])
+heatmap!(nodata(unsure_out, false), colormap=[:transparent, :orange])
+lines!(CHE, color=:black)
+contour!(distrib, color=:grey10, linewidth=1)
+hidedecorations!(current_axis())
+hidespines!(current_axis())
 current_figure()
