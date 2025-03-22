df5c8db0129ce107c3b57046f631aa67ba76d76b
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-22 10:28:38 -0400

    ah ah ah ah yeah i am unstoppable

--- main.jl.jl
+++ main.jl.jl
@@ -85,34 +85,42 @@ hyperparameters!(classifier(sdm), :interactions, :all);
 hyperparameters!(classifier(sdm), :epochs, 5_000);
 
 St, Sv = holdout(sdm)
-train!(sdm; training=St)
 
-outputs = predict(sdm; threshold=false)[Sv]
-
-conformalscore = zeros(length(outputs))
-softmaxs = _softmax.(outputs)
-for i in eachindex(conformalscore)
-    conformalscore[i] = 1 - (labels(sdm)[Sv[i]] ? softmaxs[i][1] : softmaxs[i][2])
+function _estimate_q(model, St, Sv; α=0.1, kwargs...)
+    train!(model; training=St, kwargs...)
+    f̂ = predict(model; threshold=false)[Sv]
+    𝐶 = zeros(length(f̂))
+    s = _softmax.(f̂)
+    # Conformal score from the true class label
+    for i in eachindex(𝐶)
+        𝐶[i] = 1 - (labels(model)[Sv[i]] ? s[i][1] : s[i][2])
+    end
+    # Get the quantile
+    n = length(Sv)
+    qᵢ = (n+1)*(1-α)/n
+    q̂ = quantile(𝐶, qᵢ)
+    return q̂
 end
 
-n = length(Sv)
-α = 0.05
-qi = (n+1)*(1-α)/n
-q = quantile(conformalscore, qi)
+cmodel = deepcopy(sdm)
+q = _estimate_q(cmodel, holdout(cmodel)...; α=0.05)
 
-classes = Vector{Set{Bool}}(undef, length(Sv))
-for i in eachindex(classes)
-    s = Bool[]
-    f_true, f_false = _softmax(outputs[i])
-    if f_true >= (1-q)
-        push!(s, true)
+function credibleclasses(prediction, q)
+    presence = zeros(prediction, Bool)
+    absence = zeros(prediction, Bool)
+    for k in keys(prediction)
+        s₊, s₋ = _softmax(prediction[k])
+        if s₊ >= (1-q)
+            presence[k] = true
+        end
+        if s₋ >= (1-q)
+            absence[k] = true
+        end
     end
-    if f_false >= (1-q)
-        push!(s, false)
-    end
-    classes[i] = Set(s)
+    return (presence, absence)
 end
 
-scatter(features(sdm, 1)[Sv], outputs, color=length.(classes))
-hlines!([threshold(sdm)])
-current_figure()
+prd = predict(sdm, L; threshold=false)
+
+Cp, Ca = credibleclasses(prd, q)
+heatmap(Cp .+ Ca)
